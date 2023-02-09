afc765a052bbcb1fc7546ce95e4cb6aeda2b46d9
Raluca-Mihaela Fritz <raluca.fritz@s.unibuc.ro>
2023-02-09 03:34:33 +0200

    HomePage implementation + Load from DB

--- StreamingScheduleApp/src/app/components/home/home.component.ts
+++ StreamingScheduleApp/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Stream } from 'src/types';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  streamerName: string = "";
+  allStreams!: Record<any,any>;
+  haveStreams: boolean = false;
+  keyArray: string[] = [];
+  
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
+    this.db.list('streamList').query.get().then((values)=>{
+      this.allStreams = values.val() as Record<any,any>;
+      this.keyArray = Object.keys(this.allStreams);
+      console.log(values.val());
+      this.haveStreams = true;
+    });
+  }
+
+  goToStreamLink(name: string): void{
+    location.href = `https://twitch.tv/${name}`;
   }
 
 }
