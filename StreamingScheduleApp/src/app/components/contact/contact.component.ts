5c3b27862d55c3bb9015cc37b6c58feb515ad346
Raluca-Mihaela Fritz <raluca.fritz@s.unibuc.ro>
2023-02-09 05:21:37 +0200

    Contact Save in DB+ Alerts
    Contact messages are now saved in the DB.
    Added Fail / Success Alerts when submitting a message.

--- StreamingScheduleApp/src/app/components/contact/contact.component.ts
+++ StreamingScheduleApp/src/app/components/contact/contact.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
   isList = false;
-  constructor() { }
+  addContactForm!: FormGroup;
+  username = '';
+  email = '';
+  message = '';
+  needAlert = false;
+  showAlert = false;
+
+  constructor(
+    private db: AngularFireDatabase,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.addContactForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
   }
 
+  addContact() {
+    if (this.addContactForm.valid) {
+      this.username = this.addContactForm.value.username;
+      this.email = this.addContactForm.value.email;
+      this.message = this.addContactForm.value.message;
+
+      const addContactFormData = {
+        username: this.username,
+        email: this.email,
+        message: this.message,
+      };
+
+      this.db.list('contact').push({ messages: addContactFormData });
+      console.log('contact sent to DB');
+      this.showAlert = true;
+      this.triggerAlert();
+    } else {
+      this.showAlert = false;
+      this.triggerAlert();
+      return;
+    }
+  }
+
+  triggerAlert() {
+    this.needAlert = true;
+    setTimeout(() => {
+      this.needAlert = false;
+    }, 30000);
+  }
+
+  refresh() {
+    this.router.navigate(['./dashboard']);
+  }
 }
